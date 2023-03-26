22aa2b258792ab9a70b1f228bb5b64774c9a5b94
Pr0grama <87766635+Pr0grama@users.noreply.github.com>
2023-03-26 21:54:56 +0300

    Дз 8

---
+++ Задание 8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число a: ");
+string str1 = Console.ReadLine();
+int a = Convert.ToInt32(str1);
+Console.Write("Четные числа: ");
+for (int n = 2; n <= a; n=n+2) {
+    Console.Write(n);
+    Console.Write(" ");
+}
