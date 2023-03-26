2e878f156804f32f97601756435d8661b70226d6
Pr0grama <87766635+Pr0grama@users.noreply.github.com>
2023-03-26 18:50:19 +0300

    Дз 1

---
+++ Задание 2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число a: ");
+string str1 = Console.ReadLine();
+int a = Convert.ToInt32(str1);
+Console.WriteLine("Введите число b: ");
+string str2 = Console.ReadLine();
+int b = Convert.ToInt32(str2);
+if (a > b) {
+    Console.Write("Наибольшее число ");
+    Console.WriteLine(a);
+}
+else {
+    Console.Write("Наибольшее число ");
+    Console.WriteLine(b);
+}
