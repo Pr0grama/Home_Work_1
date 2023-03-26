5d17a0e69c2e3481fd4dbc8ab18e72686875a9a2
Pr0grama <87766635+Pr0grama@users.noreply.github.com>
2023-03-26 21:29:06 +0300

    Дз 6

---
+++ Задание 6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число a: ");
+string str1 = Console.ReadLine();
+int a = Convert.ToInt32(str1);
+if (a % 2 == 0) {
+    Console.WriteLine("Четное");
+}
+else {
+    Console.WriteLine("Нечетное");
+}
