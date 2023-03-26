23a3e9dd4fefb0f0d2f930835c087df2d511b3b1
Pr0grama <87766635+Pr0grama@users.noreply.github.com>
2023-03-26 20:03:00 +0300

    Дз 4

---
+++ Задание 4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число a: ");
+string str1 = Console.ReadLine();
+int a = Convert.ToInt32(str1);
+Console.WriteLine("Введите число b: ");
+string str2 = Console.ReadLine();
+int b = Convert.ToInt32(str2);
+Console.WriteLine("Введите число c: ");
+string str3 = Console.ReadLine();
+int c = Convert.ToInt32(str3);
+int max = a;
+if ( b > max) {
+    max = b;
+}
+if (c > max) {
+    max = c;
+}
+Console.WriteLine("Наибольшее число: ");
+Console.Write(max);
