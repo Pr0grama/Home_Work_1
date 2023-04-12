8b432b4599462a982d0cd6c23838c6bd592bc413
Pr0grama <87766635+Pr0grama@users.noreply.github.com>
2023-04-12 21:45:21 +0300

    Домашнее задание 5

---
+++ HomeWork5/Task34/Program.cs
@@ -0,0 +1,36 @@
+﻿void RandomArray(int[] array, int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+
+void PrintArray(int[] array, int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void ChetNumber(int[] array, int number)
+{
+    int count = 0;
+    for (int i = 0; i < number; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count += 1;
+        }
+    }
+    Console.WriteLine($"Кол-во четных числе {count}");
+}
+
+Console.Write("Введите какого размера одномерный массив ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+RandomArray(array, number);
+PrintArray(array, number);
+ChetNumber(array, number);
---
+++ HomeWork5/Task36/Program.cs
@@ -0,0 +1,33 @@
+﻿void RandomArray(int[] array, int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = new Random().Next(-10, 11);
+    }
+}
+
+void PrintArray(int[] array, int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void SummaOddPositions(int[] array, int number)
+{
+    int summa = 0;
+    for (int i = 1; i < number; i = i + 2)
+    {
+        summa += array[i];
+    }
+    Console.WriteLine($"Сумма на нечетных позициях {summa}");
+}
+
+Console.Write("Введите какого размера одномерный массив ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+RandomArray(array, number);
+PrintArray(array, number);
+SummaOddPositions(array, number);
