97cd487e9f95c3f8807a699f89022939cdcc9ff2
Pr0grama <87766635+Pr0grama@users.noreply.github.com>
2023-04-13 21:25:01 +0300

    Добавление 38 задачи до конца

---
+++ HomeWork5/Task38/Program.cs
@@ -0,0 +1,35 @@
+﻿void RandomArray(double[] array, int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
+
+void PrintArray(double[] array, int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void MaxAndMin(double[] array, int number)
+{
+    double max = array[0];
+    double min = max;
+    for (int i = 0; i < number; i++)
+    {
+        if (max < array[i])  max = array[i];
+        if (min > array[i]) min = array[i];
+    }
+    Console.WriteLine($"Минимальный член {min}, максимальный член {max}. Их разность {max - min}");
+}
+
+Console.Write("Введите какого размера одномерный массив ");
+int number = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[number];
+RandomArray(array, number);
+PrintArray(array, number);
+MaxAndMin(array, number);
