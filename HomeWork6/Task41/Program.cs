2793d6df6209629a3c7faa5b7adfa9cd9ea69e73
Pr0grama <87766635+Pr0grama@users.noreply.github.com>
2023-04-16 20:51:06 +0300

    Home Work 6

---
+++ HomeWork6/Task41/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите числа (через пробел): ");
+int[] arr = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i]>0) count++;
+}
+Console.WriteLine(count);
---
+++ HomeWork6/Task43/Program.cs
@@ -0,0 +1,16 @@
+﻿ Console.Write($"Введите значинье b1 = ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+ Console.Write($"Введите значинье k1 = ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+ Console.Write($"Введите значинье b2 = ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+ Console.Write($"Введите значинье k2 = ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+// y = k1x + b1 = k2x + b2
+ double x = -(b1 - b2)/(k1-k2);
+ Console.WriteLine(x);
+ double y = k1*x + b1;
+Console.WriteLine(y);
+ Console.WriteLine($"({x},{y})");
