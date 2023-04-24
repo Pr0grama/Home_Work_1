6c87ae91402ee1b9f77fa9f2e2924edf7acd4cb9
Pr0grama <87766635+Pr0grama@users.noreply.github.com>
2023-04-24 18:09:58 +0300

    HomeWork8 часть 2

---
+++ Task60/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.Write("Введите x: ");
+int x = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите y: ");
+int y = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите z: ");
+int z = Convert.ToInt16(Console.ReadLine());
+
+int[,,] matrix = new int[x,y,z];
+AddArray(matrix);
+PrintArray(matrix);
+
+void PrintArray(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.WriteLine($"{matrix[i,j,k]} ({i}, {j}, {k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+void AddArray(int[,,] matrix)
+{
+    int[] number = new int[matrix.GetLength(0)*matrix.GetLength(1)*matrix.GetLength(2)];
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i,j,k] = new Random().Next(10,100);
+                number[count] = matrix[i,j,k];
+                for (int l = 0; l < count; l++)
+                {
+                    if (matrix[i,j,k] == number[count])
+                    {
+                        matrix[i,j,k] = new Random().Next(10,100);
+                        number[count] = matrix[i,j,k];
+                    }
+                }
+                count++;
+            }
+        }
+    }
+}
---
+++ Task62/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Введите кол-во строк в кв.матрице: ");
+int lines = Convert.ToInt16(Console.ReadLine());
+int[,] matrix = FillArrayRandom(lines);
+PrintArray(matrix);
+
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArrayRandom(int lines)
+{
+    int[,] matrix = new int[lines, lines];
+    int num = 0;
+    int i = 0;
+    int j = 0;
+    while (num < lines*lines)
+    {
+        matrix[i,j] = num;
+        num++;
+        if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= matrix.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > matrix.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+    return matrix;
+}
